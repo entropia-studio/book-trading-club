1a1de6f9892de7e4bf5c29ae1b36cd2961363768
entropia.studio <info@entropia.studio>
2018-08-30 18:25:14 +0200

    Environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAO2JiDQVh7XsFaGWo4TGIjhAPfAamqInw',
+    authDomain: 'angular-auth-firebase-d0ae9.firebaseapp.com',
+    databaseURL: 'https://angular-auth-firebase-d0ae9.firebaseio.com',
+    projectId: 'angular-auth-firebase-d0ae9',
+    storageBucket: '',
+    messagingSenderId: '375036189239'
+  }
+};
